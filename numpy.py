bd11eadab2054f402b5ed909ed508904b4036301
Sukhendra <developer.sukhendra@gmail.com>
2026-05-28 12:50:37 +0530

    array example

---
+++ numpy.py
@@ -0,0 +1,12 @@
+# import numpy as np
+
+# arr1 = np.array([1, 2, 3, 4, 5]) => 1D array (vector)
+# arr2 = np.array([[1, 2, 3], [4, 5, 6]]) => 2D array (matrix)
+
+# print(type(arr1)) // <class 'numpy.ndarray'>
+
+# use of np.zeros()? 
+# defination  = "np.zeros() is a function in the NumPy library that creates an array filled with zeros. You can specify the shape of the array using a tuple as an argument. For example, np.zeros((3, 4)) will create a 3x4 array filled with zeros."
+
+# arr = np.zeros((3, 4))
+# print(arr)
